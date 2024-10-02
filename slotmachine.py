d84d232d5c041dc2780a51a8c7410b9fb7636d50
tetecos <brosmega69@gmail.com>
2024-10-02 15:19:46 -0300

    Add files via upload

--- slotmachine.py
+++ slotmachine.py
@@ -14,6 +14,30 @@ symbol_count = {
     "D": 8
 }
 
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+def check_winning(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+
+    return winnings, winning_lines
+
+
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
@@ -89,8 +113,7 @@ def get_bet():
    
     return amount
 
-def main():
-    balance = deposit() 
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -107,5 +130,20 @@ def main():
 
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
+    winnings, winning_lines = check_winning(slots, lines, bet, symbol_value)
+    print(f"voce ganhou ${winnings}.")
+    print(f"voce ganhou nas linhas:", *winning_lines)
+    return winnings - total_bet
+
+def main():
+    balance = deposit() 
+    while True:
+        print(f"saldo atual equivale a ${balance}")
+        answer = input("aperte enter para jogar (aperte q para sair).")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"voce saiu com ${balance}")
 
 main() 
